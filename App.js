18bd8e58f98e292dc506aa9deea709bebd66e550
Arjun <arjun346002@gmail.com>
2023-12-29 15:57:11 +0530

    Day2

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading = React.createElement(
+  "h2",
+  { id: "head" },
+  "Hello World from React!!"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
